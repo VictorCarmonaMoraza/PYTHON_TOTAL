89c75cbf378521e01ccbf04f15309674642607a8
Victor <VictorCarmona9035@hotmail.com>
2023-06-07 21:36:36 +0200

    1-Creacion proyecto

---
+++ Dia 2/variables.py
@@ -0,0 +1,19 @@
+
+nombre = "Juan"
+print(nombre)
+
+nombre = "Laura"
+print(nombre)
+
+edad = 30
+edad2 = 15
+print(edad +edad2)
+
+#Pedir datos al usuario por teclado
+nombre = input("Dinme tu nombre")
+print("Tu nombre es "+ nombre)
+
+nombre ="hola "
+nombre2 = "Python"
+frase = nombre + nombre2
+print(frase)
