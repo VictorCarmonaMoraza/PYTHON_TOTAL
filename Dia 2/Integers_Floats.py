b13f7ec6c46d1e5cc2f789d83806cec4059fd529
Victor <VictorCarmona9035@hotmail.com>
2023-06-07 21:59:27 +0200

    2-Integer y Floats

---
+++ Dia 2/Integers_Floats.py
@@ -0,0 +1,17 @@
+mi_numero = 1
+print(mi_numero)
+
+#Imprimir tipo de la variable
+print(type(mi_numero))
+
+
+mi_numero = 5.8
+print(mi_numero)
+
+#Imprimir tipo de la variable
+print(type(mi_numero))
+
+edad = input("Dime tu edad: ")
+print("Tu edad es "+ edad)
+
+print(type(edad))
